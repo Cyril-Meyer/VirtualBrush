1dda37c5045d4eaee9cf13a690eb0096328a8f16
Cyril-Meyer <contact@cyrilmeyer.eu>
2022-07-18 09:13:12 +0200

    add brush_gui script for debug and tests

---
+++ brush_gui.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+import brush
+
+WNAME = 'VirtualBrush - brush gui'
+
+
+def mouse_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        mouse_event.paintbrush = brush.Paintbrush(position=[y, x])
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        if mouse_event.paintbrush is None:
+            return
+        DATA.fill(0)
+        mouse_event.paintbrush.move([y - mouse_event.old_y, x - mouse_event.old_x])
+        rr, cc = mouse_event.paintbrush.draw()
+        DATA[rr, cc, 0] = 255
+
+    mouse_event.old_x = x
+    mouse_event.old_y = y
+
+
+mouse_event.paintbrush = None
+mouse_event.old_x = None
+mouse_event.old_y = None
+
+
+cv2.namedWindow(WNAME, cv2.WINDOW_NORMAL)
+cv2.setMouseCallback(WNAME, mouse_event)
+
+DATA = np.zeros((512, 512, 3), dtype=np.uint8)
+while True:
+    cv2.imshow(WNAME, DATA)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
